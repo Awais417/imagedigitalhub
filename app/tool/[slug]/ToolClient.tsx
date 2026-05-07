5dc49a57db1603a16bb6195e4b519c96496eba7a
EhteshamButt <ehteshambutt58@gmail.com>
2026-05-06 22:29:50 -0800

    sa

--- app/tool/[slug]/ToolClient.tsx
+++ app/tool/[slug]/ToolClient.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import Swal from 'sweetalert2';
 import { TOOLS } from '../../lib/tools';
 
-const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'http://16.171.15.111:3000/api';
+const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'http://16.171.15.111:4000/api';
 
 export default function ToolClient({ slug }: { slug: string }) {
   const tool = TOOLS.find((t) => t.slug === slug);
